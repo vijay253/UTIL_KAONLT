1917196e40d3efdfcd5ae4d633f817219c8af6f8
Ryan-Ambrose <george.ryan.ambrose@gmail.com>
2018-10-04 03:52:07 -0400

    Modified which scripts will run.

--- scripts_KaonYield/run_KaonYield.C
+++ scripts_KaonYield/run_KaonYield.C
@@ -4,7 +4,7 @@
 #include <string>
 #include <stdio.h>
 
-void run_KaonYield(Int_t RunNumber = 0, Int_t MaxEvent = 0, Double_t threshold_cut = 0, Int_t pscal = 0)
+void run_KaonYield(Int_t RunNumber = 0, Int_t MaxEvent = 0, Double_t threshold_cut = 5, Int_t pscal = 1)
 {
   // Get RunNumber, MaxEvent, and current threshold if not provided.
   if(RunNumber == 0) {
@@ -59,8 +59,8 @@ void run_KaonYield(Int_t RunNumber = 0, Int_t MaxEvent = 0, Double_t threshold_c
   ch.SetProof();
   ch.Process("KaonYield.C+",option);
   proof->Close();
-
+  
   TChain sc("TSH");
   sc.Add(Form("/home/cdaq/hallc-online/hallc_replay/UTIL_KAONLT/ROOTfiles/KaonLT_coin_replay_production_%i_%i.root",RunNumber,MaxEvent));
-  sc.Process("HMS_Scalers.C+",threshold);
+  sc.Process("HMS_Scalers.C+",option);
 }
