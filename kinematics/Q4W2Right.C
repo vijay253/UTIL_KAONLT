4055c9f1ececf43237a67a0e233b511b2207fcc7
Stephen Kay <sjdkay@jlab.org>
2019-05-24 16:48:30 -0400

    Fixed pathing of scripts, now point to new replay directory. Removed some outdated files.

--- kinematics/Q4W2Right.C
+++ kinematics/Q4W2Right.C
@@ -7,22 +7,22 @@
 void Q4W2Right()
 {
   TChain ch("T");
-  ch.Add("/u/group/c-kaonlt/hallc_replay_kaonlt/UTIL_KAONLT/ROOTfiles/KaonLT_coin_replay_production_4965_-1.root");
-  ch.Add("/u/group/c-kaonlt/hallc_replay_kaonlt/UTIL_KAONLT/ROOTfiles/KaonLT_coin_replay_production_4966_-1.root");
-  ch.Add("/u/group/c-kaonlt/hallc_replay_kaonlt/UTIL_KAONLT/ROOTfiles/KaonLT_coin_replay_production_4967_-1.root");
-  ch.Add("/u/group/c-kaonlt/hallc_replay_kaonlt/UTIL_KAONLT/ROOTfiles/KaonLT_coin_replay_production_4968_-1.root");
-  ch.Add("/u/group/c-kaonlt/hallc_replay_kaonlt/UTIL_KAONLT/ROOTfiles/KaonLT_coin_replay_production_4969_-1.root");
-  ch.Add("/u/group/c-kaonlt/hallc_replay_kaonlt/UTIL_KAONLT/ROOTfiles/KaonLT_coin_replay_production_4970_-1.root");
-  ch.Add("/u/group/c-kaonlt/hallc_replay_kaonlt/UTIL_KAONLT/ROOTfiles/KaonLT_coin_replay_production_4971_-1.root");
-  ch.Add("/u/group/c-kaonlt/hallc_replay_kaonlt/UTIL_KAONLT/ROOTfiles/KaonLT_coin_replay_production_4972_-1.root");
-  ch.Add("/u/group/c-kaonlt/hallc_replay_kaonlt/UTIL_KAONLT/ROOTfiles/KaonLT_coin_replay_production_4973_-1.root");
-  ch.Add("/u/group/c-kaonlt/hallc_replay_kaonlt/UTIL_KAONLT/ROOTfiles/KaonLT_coin_replay_production_4974_-1.root");
-  ch.Add("/u/group/c-kaonlt/hallc_replay_kaonlt/UTIL_KAONLT/ROOTfiles/KaonLT_coin_replay_production_4975_-1.root");
+  ch.Add("/u/group/c-kaonlt/hallc_replay_lt/UTIL_KAONLT/ROOTfiles/KaonLT_coin_replay_production_4965_-1.root");
+  ch.Add("/u/group/c-kaonlt/hallc_replay_lt/UTIL_KAONLT/ROOTfiles/KaonLT_coin_replay_production_4966_-1.root");
+  ch.Add("/u/group/c-kaonlt/hallc_replay_lt/UTIL_KAONLT/ROOTfiles/KaonLT_coin_replay_production_4967_-1.root");
+  ch.Add("/u/group/c-kaonlt/hallc_replay_lt/UTIL_KAONLT/ROOTfiles/KaonLT_coin_replay_production_4968_-1.root");
+  ch.Add("/u/group/c-kaonlt/hallc_replay_lt/UTIL_KAONLT/ROOTfiles/KaonLT_coin_replay_production_4969_-1.root");
+  ch.Add("/u/group/c-kaonlt/hallc_replay_lt/UTIL_KAONLT/ROOTfiles/KaonLT_coin_replay_production_4970_-1.root");
+  ch.Add("/u/group/c-kaonlt/hallc_replay_lt/UTIL_KAONLT/ROOTfiles/KaonLT_coin_replay_production_4971_-1.root");
+  ch.Add("/u/group/c-kaonlt/hallc_replay_lt/UTIL_KAONLT/ROOTfiles/KaonLT_coin_replay_production_4972_-1.root");
+  ch.Add("/u/group/c-kaonlt/hallc_replay_lt/UTIL_KAONLT/ROOTfiles/KaonLT_coin_replay_production_4973_-1.root");
+  ch.Add("/u/group/c-kaonlt/hallc_replay_lt/UTIL_KAONLT/ROOTfiles/KaonLT_coin_replay_production_4974_-1.root");
+  ch.Add("/u/group/c-kaonlt/hallc_replay_lt/UTIL_KAONLT/ROOTfiles/KaonLT_coin_replay_production_4975_-1.root");
 
   TProof *proof = TProof::Open("workers=8");
   //proof->SetProgressDialog(0);  
   ch.SetProof();
-  ch.Process("/u/group/c-kaonlt/hallc_replay_kaonlt/UTIL_KAONLT/scripts_KaonYield/KaonYield_Q3W2Center.C+","1");
+  ch.Process("/u/group/c-kaonlt/hallc_replay_lt/UTIL_KAONLT/scripts_KaonYield/KaonYield_Q3W2Center.C+","1");
   proof->Close();
   
 }
