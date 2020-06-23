af7490bdd51b1c38b0128e597fb974e01740c433
Stephen Kay <sjdkay@jlab.org>
2020-06-23 13:12:44 -0400

    Moved kinematics scripts from within kaonyield to a new folder. Moved outdated ones to a legacy folder.

--- scripts/kaonyield/Analyse_Kaons.sh
+++ scripts/kaonyield/Analyse_Kaons.sh
@@ -21,14 +21,10 @@ fi
 # Set path depending upon hostname. Change or add more as needed  
 if [[ "${HOSTNAME}" = *"farm"* ]]; then  
     REPLAYPATH="/group/c-kaonlt/USERS/${USER}/hallc_replay_lt"
-    if [[ "${HOSTNAME}" != *"ifarm"* ]]; then
-	source /site/12gev_phys/softenv.sh 2.3
-    fi
     cd "$REPLAYPATH"
     source "$REPLAYPATH/setup.sh"
 elif [[ "${HOSTNAME}" = *"qcd"* ]]; then
     REPLAYPATH="/group/c-kaonlt/USERS/${USER}/hallc_replay_lt"
-    source /site/12gev_phys/softenv.sh 2.3
     cd "$REPLAYPATH"
     source "$REPLAYPATH/setup.sh" 
 elif [[ "${HOSTNAME}" = *"cdaq"* ]]; then
