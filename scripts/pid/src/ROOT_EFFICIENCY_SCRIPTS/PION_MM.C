b8e6217e4c811379691f2d5b97b99767c055f725
Vijay KUMAR <vijay@ifarm1901.jlab.org>
2020-12-07 13:02:54 -0500

    PION_MM.C script added to the timing info plots

--- scripts/pid/src/ROOT_EFFICIENCY_SCRIPTS/PION_MM.C
+++ scripts/pid/src/ROOT_EFFICIENCY_SCRIPTS/PION_MM.C
@@ -81,10 +81,12 @@ void PION_MM(string InFilename = "", string OutFilename = "")
   
  // Particles information with "PION" pid
   TTree* Pion_No_HGC_Cut  = (TTree*)InFile->Get("SHMS_Pions_Without_HGC_Cuts"); Long64_t nEntries_Pion_No_HGC_Cut  = (Long64_t)Pion_No_HGC_Cut->GetEntries();
-
+  Double_t P_CTime_ePion;Pion_No_HGC_Cut->SetBranchAddress("CTime_ePiCoinTime_ROC1",&P_CTime_ePion);
+  Double_t P_RF_tdcTime;Pion_No_HGC_Cut->SetBranchAddress("P_RF_tdcTime", &P_RF_tdcTime);
   Double_t P_hgcer_xAtCer;Pion_No_HGC_Cut->SetBranchAddress("P_hgcer_xAtCer", &P_hgcer_xAtCer);
   Double_t P_hgcer_yAtCer;Pion_No_HGC_Cut->SetBranchAddress("P_hgcer_yAtCer", &P_hgcer_yAtCer);
   Double_t P_aero_yAtCer;Pion_No_HGC_Cut->SetBranchAddress("P_aero_yAtCer", &P_aero_yAtCer);
+  Double_t P_gtr_dp; Pion_No_HGC_Cut->SetBranchAddress("P_gtr_dp", &P_gtr_dp);
   Double_t Pion_npeSum;Pion_No_HGC_Cut->SetBranchAddress("P_hgcer_npeSum", &Pion_npeSum);
   Double_t Pion_aero_npeSum;Pion_No_HGC_Cut->SetBranchAddress("P_aero_npeSum", &Pion_aero_npeSum);
   Double_t Pion_gtr_p; Pion_No_HGC_Cut->SetBranchAddress("P_gtr_p", &Pion_gtr_p);
@@ -153,8 +155,15 @@ void PION_MM(string InFilename = "", string OutFilename = "")
   TH1D *h1_MM_Pi_IN_TCutG1     = new TH1D("h1_MM_Pi_IN_TCutG1","Pion Missing Mass; Missing Mass (Pion); Events;", 300, 0.5, 2.0 ); 
   TH1D *h1_MM_Pi_IN_TCutG12    = new TH1D("h1_MM_Pi_IN_TCutG12","Pion Missing Mass; Missing Mass (Pion); Events;", 300, 0.5, 2.0 );
   TH1D *h1_MM_Pi_OUT_TCutG2    = new TH1D("h1_MM_Pi_OUT_TCutG2","Pion Missing Mass; Missing Mass (Pion); Events;", 300, 0.5, 2.0 );
- 
- 
+
+  // Histogrms for Coin and RF timing info
+  TH1D *h1_CTime_ePion_IN_TCutG1         = new TH1D("h1_CTime_ePion_IN_TCutG1","Electron Pion Coin Time; CTime_ePiCoinTime_ROC1; Events;", 300, 0.0, 100.0 ); 
+  TH1D *h1_CTime_ePion_IN_TCutG12        = new TH1D("h1_CTime_ePion_IN_TCutG12","Electron Pion Coin Time; CTime_ePiCoinTime_ROC1; Events;", 300, 0.0, 100.0 ); 
+  TH1D *h1_CTime_ePion_OUT_TCutG2        = new TH1D("h1_CTime_ePion_OUT_TCutG2","Electron Pion Coin Time; CTime_ePiCoinTime_ROC1; Events;", 300, 0.0, 100.0 ); 
+  TH1D *h1_RF_tdc_Time_IN_TCutG1        = new TH1D("h1_RF_tdc_Time_IN_TCutG1","RF Timing Info; P_RF_tdcTime; Events;", 300, -100, 100 ); 
+  TH1D *h1_RF_tdc_Time_IN_TCutG12        = new TH1D("h1_RF_tdc_Time_IN_TCutG12","RF Timing Info; P_RF_tdcTime; Events;", 300, -100, 100 ); 
+  TH1D *h1_RF_tdc_Time_OUT_TCutG2        = new TH1D("h1_RF_tdc_Time_OUT_TCutG2","RF Timing Info; P_RF_tdcTime; Events;", 300, -100, 100); 
+
   //Fill entries for missing masses
 
   for(Long64_t i = 0; i < nEntries_Pion_No_HGC_Cut; i++){
@@ -162,19 +171,22 @@ void PION_MM(string InFilename = "", string OutFilename = "")
     if (!cutg1->IsInside(P_hgcer_yAtCer, P_hgcer_xAtCer)) continue;
     if(Pion_npeSum <1.0 || Pion_aero_npeSum <1.0 || P_aero_yAtCer >31) continue;
     h1_MM_Pi_IN_TCutG1->Fill(sqrt(pow((Pion_e_miss + (sqrt((MK*MK) + (Pion_gtr_p*Pion_gtr_p))) - (sqrt((MPi*MPi) + (Pion_gtr_p*Pion_gtr_p)))), 2) - (Pion_p_miss*Pion_p_miss)));
+    h1_CTime_ePion_IN_TCutG1->Fill(P_CTime_ePion);
+    h1_RF_tdc_Time_IN_TCutG1->Fill(P_RF_tdcTime);
+
   }
   
-  //Fill NPE entry inside the TCutG2      
+  //Fill entry outside the TCutG2
   
   for(Long64_t i = 0; i < nEntries_Pion_No_HGC_Cut; i++){
     Pion_No_HGC_Cut->GetEntry(i);
     if (cutg2->IsInside(P_hgcer_yAtCer, P_hgcer_xAtCer)) continue;
     if(Pion_npeSum < 3.5 || Pion_aero_npeSum <1.0 || P_aero_yAtCer >31) continue;
     h1_MM_Pi_OUT_TCutG2->Fill(sqrt(pow((Pion_e_miss + (sqrt((MK*MK) + (Pion_gtr_p*Pion_gtr_p))) - (sqrt((MPi*MPi) + (Pion_gtr_p*Pion_gtr_p)))), 2) - (Pion_p_miss*Pion_p_miss)));
+    h1_CTime_ePion_OUT_TCutG2->Fill(P_CTime_ePion);
+    h1_RF_tdc_Time_OUT_TCutG2->Fill(P_RF_tdcTime);
   }  
  
-  //Fill entry outside the TCutG2
-
   //Fill entry between TCutG1 & 2
   
   for(Long64_t i = 0; i < nEntries_Pion_No_HGC_Cut; i++){
@@ -183,8 +195,11 @@ void PION_MM(string InFilename = "", string OutFilename = "")
     if (cutg1->IsInside(P_hgcer_yAtCer, P_hgcer_xAtCer)) continue;
     if(Pion_npeSum < 1.0 || Pion_aero_npeSum <1.0 || P_aero_yAtCer >31) continue;
     h1_MM_Pi_IN_TCutG12->Fill(sqrt(pow((Pion_e_miss + (sqrt((MK*MK) + (Pion_gtr_p*Pion_gtr_p))) - (sqrt((MPi*MPi) + (Pion_gtr_p*Pion_gtr_p)))), 2) - (Pion_p_miss*Pion_p_miss)));
+    h1_CTime_ePion_IN_TCutG12->Fill(P_CTime_ePion);
+    h1_RF_tdc_Time_IN_TCutG12->Fill(P_RF_tdcTime);
+
   }
- 
+
   //Write the info in the root format
 
   TFile *OutHisto_file = new TFile(foutname,"RECREATE");
@@ -193,6 +208,13 @@ void PION_MM(string InFilename = "", string OutFilename = "")
   h1_MM_Pi_IN_TCutG1->Write();
   h1_MM_Pi_IN_TCutG12->Write();
   h1_MM_Pi_OUT_TCutG2->Write();
+  h1_CTime_ePion_IN_TCutG1->Write();
+  h1_RF_tdc_Time_IN_TCutG1->Write(); 
+  h1_CTime_ePion_IN_TCutG12->Write();
+  h1_RF_tdc_Time_IN_TCutG12->Write(); 
+  h1_CTime_ePion_OUT_TCutG2->Write();
+  h1_RF_tdc_Time_OUT_TCutG2->Write(); 
+
   OutHisto_file->Close();
 
   // TString RunNumStr = TInFilename(0,4); Int_t RunNum=(RunNumStr.Atoi());
