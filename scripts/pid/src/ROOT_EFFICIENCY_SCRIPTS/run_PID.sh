e7aa1718f6c91ec532b0e9d89ad5545c35928d38
Vijay KUMAR <vijay@ifarm1801.jlab.org>
2021-05-26 15:16:03 -0400

    run_PID.sh script updated

--- scripts/pid/src/ROOT_EFFICIENCY_SCRIPTS/run_PID.sh
+++ scripts/pid/src/ROOT_EFFICIENCY_SCRIPTS/run_PID.sh
@@ -16,6 +16,21 @@ if [[ "${HOSTNAME}" = *"farm"* ]]; then
     cd "$REPLAYPATH"
     source "$REPLAYPATH/setup.sh"
 fi
+#To run the SHMS_pid.C script
 
-cd ${REPLAYPATH}/UTIL_KAONLT/scripts/pid/src/PYTHON_EFFICIENCY_SCRIPTS/OUTPUT/
-root -b -q  "${REPLAYPATH}/UTIL_KAONLT/scripts/pid/src/ROOT_EFFICIENCY_SCRIPTS/SHMS_pid.C(\"PID_E_8p18_Q2_3p0_W_3p14_P_shms_6p053_low_epsilo.root\", \"PID_E_8p18_Q2_3p0_W_3p14_P_shms_6p053_low_epsilo_OUT\")"
+#cd ${REPLAYPATH}/UTIL_KAONLT/scripts/pid/src/ROOT_EFFICIENCY_SCRIPTS/
+#root -b -q "SHMS_pid.C(\"PID_E_8p18_Q2_3p0_W_3p14_P_shms_6p053_low_epsilon.root\", \"PID_E_8p18_Q2_3p0_W_3p14_P_shms_6p053_low_epsilon_OUT\")"
+
+#Run EFFICIENCY.C script 
+
+cd ${REPLAYPATH}/UTIL_KAONLT/scripts/pid/src/ROOT_EFFICIENCY_SCRIPTS/
+root -b -q  "SCRIPT_PION_PID.C(\"PID_E_8p18_Q2_3p0_W_3p14_P_shms_6p053_low_epsilon.root\", \"PION_PID_8p18_Q2_3p0_W_3p14_P_shms_6p053_low_epsilon_OUT\")"
+root -b -q  "SCRIPT_KAON_PID.C(\"PID_E_8p18_Q2_3p0_W_3p14_P_shms_6p053_low_epsilon.root\", \"KAON_PID_8p18_Q2_3p0_W_3p14_P_shms_6p053_low_epsilon_OUT\")"
+#root -b -q "PID_SCRIPT_PASS2.C(\"pid_8046_-1.root\", \"PID_8046_OUT\")"
+#root -b -q "SCRIPT_PION_PID.C(\"pid_8046_-1.root\", \"PION_PID_8046_OUT\")"
+#root -b -q "SCRIPT_KAON_PID.C(\"pid_8046_-1.root\", \"KAON_PID_8046_OUT\")"
+
+#To run the chain script
+
+#cd ${REPLAYPATH}/UTIL_KAONLT/scripts/pid/src/ROOT_EFFICIENCY_SCRIPTS/
+#root -b -q chain_files.C
